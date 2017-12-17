44ba5b87df0e671718758218893122f288e86ad2
Hannah Bare <johnd>
2017-12-17 13:28:49 -0700

    Refactor js and create object

--- scripts.js
+++ scripts.js
@@ -5,31 +5,41 @@ var $appendHere = $('.append-here');
 var $deleteBtn = $('#delete-btn');
 
 
-$saveBtn.on('click', function(event){
-  event.preventDefault();
-  $appendHere.append(`<article class="cards">
+function NewCard (title, body, id){
+  this.title = title;
+  this.body = body;
+  this.id = id;
+  this.quality = 'swill';
+}
+
+NewCard.prototype.prependCard = function() {
+   $appendHere.prepend(`<article class="cards">
     <button class="top-card card-button" id="delete-btn"></button>
-    <h3 class="top-card">${$titleInput.val()}<span class="card-counter">1</span>
-    </h3>
-    <p>${$bodyInput.val()}</p>
+    <h3 class="top-card">${this.title}</h3>
+    <p>${this.body}</p>
     <button class="card-button bottom-line" id="upvote-btn"></button>
     <button class="card-button bottom-line"
     id="downvote-btn"></button>
-    <h6 class="bottom-line">quality:<span class="quality-change"> swill</span></h6>
+    <h6 class="bottom-line">quality:<span class="quality-change">${this.quality}</span></h6>
     <hr>
     </article>`);
-  $('#input-title').empty();
-  $('#input-body').empty();
+};
+
+$saveBtn.on('click', function(event){
+  event.preventDefault();
+  var id = Date.now();
+  var card = new NewCard ($titleInput.val(), $bodyInput.val(), id);
+  card.prependCard();
+  $titleInput.val("");
+  $bodyInput.val("");
 });
 
 $appendHere.on('click', '#delete-btn', function(event) {
     $(this).parent().remove();
- // remove();
 });
-// });
-// $deleteBtn.on('click', deleteCard);
 
-// function deleteCard() {
-//   console.log('hello');
-//   $(this).parent().remove();
-// }
+// $appendHere.on('click', 'h3', function(event){
+//   console.log(('h3').text);
+//   var $cardTitle = $('.top-card');
+//   editContent.setAttribute('contenteditable', true);
+// })
