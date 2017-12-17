ceca69ac3cd71a6bb0a60098be51c5a9990186ed
Ngoc Phan <nphan4@yahoo.com>
2017-12-17 14:27:51 -0700

    Create object and got all buttons to work

--- scripts.js
+++ scripts.js
@@ -3,23 +3,22 @@ var $bodyInput = $('#input-body');
 var $saveBtn = $('#save-btn');
 var $appendHere = $('.append-here');
 var $deleteBtn = $('#delete-btn');
-
+// var $upVote = $('#upvote-btn');
 
 function NewCard (title, body, id){
   this.title = title;
   this.body = body;
   this.id = id;
-  this.quality = 'swill';
+  this.quality = ' swill';
 }
 
 NewCard.prototype.prependCard = function() {
    $appendHere.prepend(`<article class="cards">
     <button class="top-card card-button" id="delete-btn"></button>
-    <h3 class="top-card">${this.title}</h3>
-    <p>${this.body}</p>
+    <h3 class="top-card" contenteditable=true>${this.title}</h3>
+    <p contenteditable=true>${this.body}</p>
     <button class="card-button bottom-line" id="upvote-btn"></button>
-    <button class="card-button bottom-line"
-    id="downvote-btn"></button>
+    <button class="card-button bottom-line" id="downvote-btn"></button>
     <h6 class="bottom-line">quality:<span class="quality-change">${this.quality}</span></h6>
     <hr>
     </article>`);
@@ -35,11 +34,19 @@ $saveBtn.on('click', function(event){
 });
 
 $appendHere.on('click', '#delete-btn', function(event) {
-    $(this).parent().remove();
+  $(this).parent().remove();
 });
 
-// $appendHere.on('click', 'h3', function(event){
-//   console.log(('h3').text);
-//   var $cardTitle = $('.top-card');
-//   editContent.setAttribute('contenteditable', true);
-// })
+$appendHere.on('click', '#upvote-btn', function(event) { 
+  if($(this).siblings('h6').children('span').text() === ' swill'){
+    $(this).siblings('h6').children('span').text(' plausible');
+  } else if ($(this).siblings('h6').children('span').text() === ' plausible') {
+    $(this).siblings('h6').children('span').text(' genius')}
+});
+
+$appendHere.on('click', '#downvote-btn', function(event) { 
+  if($(this).siblings('h6').children('span').text() === ' genius'){
+    $(this).siblings('h6').children('span').text(' plausible');
+  } else if ($(this).siblings('h6').children('span').text() === ' plausible') {
+    $(this).siblings('h6').children('span').text(' swill')}
+});
